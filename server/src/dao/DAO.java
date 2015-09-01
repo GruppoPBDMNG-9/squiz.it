19f4ee9df848cd42dd35bb2c1a879b713e00196e
Corrado Giancaspro <c.giancaspro5@gmail.com>
2015-09-01 14:24:36 +0200

    implementation added

--- server/src/dao/DAO.java
+++ server/src/dao/DAO.java
@@ -1,4 +1,12 @@
 package dao;
 
+import com.datastax.driver.core.BoundStatement;
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
 public abstract class DAO {
+
 }
