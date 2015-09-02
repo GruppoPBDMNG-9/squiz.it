4e67fa5960eeb10bb5a6c13bc0b283e42727d474
Giuseppe Lorusso <giuseppelorusso92@gmail.com>
2015-09-02 16:26:51 +0200

    comment

--- server/src/dao/DAO.java
+++ server/src/dao/DAO.java
@@ -1,11 +1,13 @@
 package dao;
 
+/*
 import com.datastax.driver.core.BoundStatement;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+*/
 
 public abstract class DAO {
 
